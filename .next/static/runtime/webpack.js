329eef73c0dbaea325b732f55b9adf206b5a5fd8
Developer <Dev@jacobs-macbook-pro.home>
2020-11-17 09:55:16 +0100

    fix links on mobile menu

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b3d42c808a0ddcad3ce4";
+/******/ 	var hotCurrentHash = "9ec18da351c6ef7b1a94";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- components/sideMenu.js
+++ components/sideMenu.js
@@ -1,6 +1,6 @@
-import React from "react";
-import Link from 'next/link'
-import auth0 from '../services/auth0'
+import React, { useState } from 'react'
+import { useRouter } from 'next/router'
+import auth0 from '../services/auth0';
 import { stack as Menu } from "react-burger-menu";
 
 var styles = {
@@ -22,75 +22,77 @@ var styles = {
     width: '24px'
   },
   bmCross: {
-    background: '#bdc3c7'
+    background: '#ffffff'
   },
   bmMenuWrap: {
     position: 'fixed',
     height: '100%',
-    top: '-0px'
+    top: '-0px',
   },
   bmMenu: {
-    background: '#0b334d',
-    padding: '2.5em 1.5em 0',
-    fontSize: '1.15em',
-    border: '3px solid #cccccc'
+    background: '#494949',
+    padding: '',
+    fontSize: '1.6em',
   },
   bmMorphShape: {
     fill: '#373a47'
   },
   bmItemList: {
     color: '#b8b7ad',
-    padding: '0.8em'
+    padding: '1em 0 0 0.8em',
   },
   bmItem: {
-    display: 'block'
+    display: 'block',
+    padding: '0.2em 0',
+    color: 'white',
+    fontFamily: 'Paytone One',
+    border: '0',
+    outline: 'none'
   },
   bmOverlay: {
-    background: 'rgba(0, 0, 0, 0)'
+    background: 'rgba(0, 0, 0, 0)',
   },
-
 }
 
 const SideMenu = () => {
 
+  const [menuOpen, setMenuOpen] = useState(false);
+  const router = useRouter()
+  const home = () => {
+    router.push('/')
+    setMenuOpen(false)
+  }
+  const myActivities = () => {
+    router.push('/my-activities')
+    setMenuOpen(false)
+  }
+  const addActivity = () => {
+    router.push('/create')
+    setMenuOpen(false)
+  }
 
   return (
-
     <>
+      { auth0.isAuthenticated() === false &&
+        <Menu className="burger" right styles={ styles } width={ '220px' } isOpen={ menuOpen }>
+        <a href="/">Home</a>
+        <a onClick={auth0.loginAddActivity}>Add activity</a>
+        <a onClick={auth0.login}>Sign in</a>
+        </Menu>
+      }
 
-{ auth0.isAuthenticated() &&
-  <Menu className="burger" right styles={ styles }>
-    <a href="/">Home</a>
-    <a href="/create">Add activity</a>
-    <a href="/my-activities">My activities</a>
-    <a onClick={auth0.logout}>Logout</a>
-  </Menu>
-
-  }
-
-{ auth0.isAuthenticated() === false &&
-<Menu className="burger" right styles={ styles }>
-<a href="/">Home</a>
-<a onClick={auth0.loginAddActivity}>Add activity</a>
-<a onClick={auth0.login}>Sign in</a>
-</Menu>
-
-  }
-
-  <style jsx>{`
-  .menu-item {
-    display: block;
-  }
-`}</style>
+      { auth0.isAuthenticated() &&
+        <Menu className="burger" right styles={ styles } width={ '220px' } isOpen={ menuOpen }>
+        <a onClick={home}>Home</a>
+          <a onClick={myActivities}>My activities</a>
+          <a onClick={addActivity}>Add activity</a>
+          <a onClick={auth0.logout}>Logout</a>
+        </Menu>
+      }
 
 
-
-
-
-
-</>
+    </>
   )
-
 };
 
 export default SideMenu;
